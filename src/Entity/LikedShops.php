b85bfba3a7a33273a834534762fad58290288995
Hamza Hasbi <hamza.hasbi@gmail.com>
2019-10-18 01:18:56 +0100

    Config: framework && routes config + minor entity change

--- src/Entity/LikedShops.php
+++ src/Entity/LikedShops.php
@@ -7,6 +7,12 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\LikedShopsRepository")
+ * @ORM\Table(name="liked_shop",
+ *    uniqueConstraints={
+ *        @ORM\UniqueConstraint(name="liked_shops_unique",
+ *            columns={"shops_id", "user_id"})
+ *    }
+ * )
  */
 class LikedShops
 {
@@ -35,5 +41,56 @@ class LikedShops
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function addShop(Shop $shop): self
+    {
+        if (!$this->shops->contains($shop)) {
+            $this->shops[] = $shop;
+        }
+        return $this;
+    }
+    public function removeShop(Shop $shop): self
+    {
+        if ($this->shops->contains($shop)) {
+            $this->shops->removeElement($shop);
+        }
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShops()
+    {
+        return $this->shops;
+    }
+
+    /**
+     * @param mixed $shops
+     * @return LikedShops
+     */
+    public function setShops($shops): self
+    {
+        $this->shops = $shops;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     * @return LikedShops
+     */
+    public function setUser($user): self
+    {
+        $this->user = $user;
+        return $this;
     }
 }
