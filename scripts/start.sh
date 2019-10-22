7845ddf2547d7cecc058093096823df1e30f7fec
Hamza Hasbi <hamza.hasbi@gmail.com>
2019-10-22 16:08:35 +0100

    Update: Added Folder creation

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mkdir config/jwt
 openssl genpkey -out config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096 -pass pass:webcodingchallengeapi
 openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout -passin pass:webcodingchallengeapi
 php bin/console doctrine:database:create
